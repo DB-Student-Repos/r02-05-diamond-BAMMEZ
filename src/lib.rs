0d5d278c65b368de4ef03487a8086f1298cd3e8a
Bammez <Kingnuel4@gmail.com>
2024-05-03 15:00:06 +0200

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,20 @@
 pub fn get_diamond(c: char) -> Vec<String> {
-    unimplemented!(
-        "Return the vector of strings which represent the diamond with particular char {c}"
-    );
+    let mut diamond: Vec<String> = Vec::new();
+    let size = (c as u8 - b'A' + 1) as usize;
+    
+    for i in 0..size {
+        let mut row = vec![' '; size * 2 - 1];
+        let ch = (b'A' + i as u8) as char;
+        let index = size - 1 - i;
+        row[index] = ch;
+        row[size + i - 1] = ch;
+        diamond.push(row.iter().collect());
+    }
+
+    for i in (0..size - 1).rev() {
+        let row = diamond[i].clone();
+        diamond.push(row);
+    }
+
+    diamond
 }
